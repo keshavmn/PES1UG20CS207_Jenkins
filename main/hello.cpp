da9af51d1a3157dab22e71bef95a9468192cc939
keshavmn <111689175+keshavmn@users.noreply.github.com>
2023-02-17 10:00:52 +0530

    Add files via upload

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG20CS207,I have successfully built and run\n";
+  return 0;
+}
